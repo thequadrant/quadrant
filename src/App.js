fdd8266dad7f22c7cc1b59e8eebad048abfca299
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-03-06 23:41:43 +0800

    Add react routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./views/AboutUs";
 import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
