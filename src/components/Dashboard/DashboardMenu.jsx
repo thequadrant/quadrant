56277d728b054b6646f928dc1e47e29628e7625a
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-03-18 21:45:36 +0800

    Remove console.log statements

--- src/components/Dashboard/DashboardMenu.jsx
+++ src/components/Dashboard/DashboardMenu.jsx
@@ -18,12 +18,6 @@ function DashboardMenu() {
       style={{
         background: colorBgContainer,
       }}
-      onBreakpoint={(broken) => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
     >
       <div className="demo-logo-vertical" />
       <Menu
