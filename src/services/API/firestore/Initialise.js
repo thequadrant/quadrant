68fcd53fabf38dba46efbb02a50ff33712ebec82
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-03-20 21:22:48 +0800

    add config details

--- src/services/API/firestore/Initialise.js
+++ src/services/API/firestore/Initialise.js
@@ -2,8 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Firebase config
+
+  apiKey: "AIzaSyDHWa6jBnLadN-O_a3F5-lZq0y5uzTvW9U",
+
+  authDomain: "quadrant-dev.firebaseapp.com",
+
+  projectId: "quadrant-dev",
+
+  storageBucket: "quadrant-dev.appspot.com",
+
+  messagingSenderId: "764599056515",
+
+  appId: "1:764599056515:web:78c5761c2f074cf24543b6",
+
+  measurementId: "G-BJ4JE1FXMF",
+
 };
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
