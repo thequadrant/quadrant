a46a26c32dd8bb4c945bc31afe6fb8ca9ee10e2b
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-03-21 16:34:27 +0800

    Change to use env secrets

--- src/services/API/firestore/Initialise.js
+++ src/services/API/firestore/Initialise.js
@@ -2,21 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDHWa6jBnLadN-O_a3F5-lZq0y5uzTvW9U",
-
-  authDomain: "quadrant-dev.firebaseapp.com",
-
-  projectId: "quadrant-dev",
-
-  storageBucket: "quadrant-dev.appspot.com",
-
-  messagingSenderId: "764599056515",
-
-  appId: "1:764599056515:web:78c5761c2f074cf24543b6",
-
-  measurementId: "G-BJ4JE1FXMF",
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
